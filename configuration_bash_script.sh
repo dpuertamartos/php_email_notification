d92aeb6f50f4bdae8afd4daee270d47eaa6fb406
David Puerta Martos <92330952+dpuertamartos@users.noreply.github.com>
2023-08-26 19:25:55 +0200

    Update configuration_bash_script.sh with php7.4-curl

--- configuration_bash_script.sh
+++ configuration_bash_script.sh
@@ -3,7 +3,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y software-properties-common apt-transport-https
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
-sudo apt install -y php7.4 php7.4-cli php7.4-json php7.4-common php7.4-mysql php7.4-zip php7.4-fpm php7.4-mbstring php7.4-xml
+sudo apt install -y php7.4 php7.4-curl php7.4-cli php7.4-json php7.4-common php7.4-mysql php7.4-zip php7.4-fpm php7.4-mbstring php7.4-xml
 php -v
 cd ~
 curl -sS https://getcomposer.org/installer -o composer-setup.php
